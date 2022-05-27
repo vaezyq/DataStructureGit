baf1b49309bf7f90b31b370c57a15c91a5a080ed
22126 <2212688995@qq.com>
2022-05-27 18:15:14 +0800

    first initally

---
+++ Vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 22126 on 2022/5/27.
+//
+
+#include "Vector.h"
---
+++ Vector.h
@@ -0,0 +1,14 @@
+//
+// Created by 22126 on 2022/5/27.
+//
+
+#ifndef DATASTRUCTUREGIT_VECTOR_H
+#define DATASTRUCTUREGIT_VECTOR_H
+
+
+class Vector {
+
+};
+
+
+#endif //DATASTRUCTUREGIT_VECTOR_H
